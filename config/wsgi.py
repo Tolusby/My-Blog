81615292dcc68f3bd418ad17bf057a840bee04bc
Sobayo Tolu <toluwaso@outlook.com>
2021-05-07 23:21:48 +0100

    testing

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if path not in sys.path:
+    sys.path.append(path)
+    
 application = get_wsgi_application()
